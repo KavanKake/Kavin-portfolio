156b90b5318c55708698d4764a245e5782feb880
KavanKake <Kavin_lokeswaran1407@outlook.com>
2024-11-05 12:20:18 +0100

    changes

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<nav>
+    <ul>
+        <li>
+            <a href="/">home</a>
+        </li>
+        <li>
+            <a href="/">Projects</a>
+        </li>
+        <li>
+            <a href="/">About me</a>
+        </li>
+        <li>
+            <a href="/">Extra pages</a>
+        </li>
+        <li>
+            <a href="/">Contact me</a>
+        </li>
+    </ul>
+</nav>
+
+
+
+
+
+home
---
+++ my-app/src/routes/extrapages/+page.svelte
@@ -0,0 +1,1 @@
+<h1> This is the extra pages page</h1>
