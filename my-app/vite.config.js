ffa772412b88ccdf653b720a53d670adfa4c6e55
KavanKake <Kavin_lokeswaran1407@outlook.com>
2024-12-06 10:39:19 +0100

    Update vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,4 +3,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+<<<<<<< HEAD
+=======
+	base: '/Kavin-portfolio/',
+>>>>>>> parent of ce8baf0 (Update vite.config.js)
 });
